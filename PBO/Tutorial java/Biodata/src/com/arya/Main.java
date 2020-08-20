9dd643253d7eb60cb58596c73b121c43be933ab9
aryayudhapratama <69952993+aryayudhapratama@users.noreply.github.com>
2020-08-20 17:06:08 +0900

    Add files via upload

---
+++ PBO/Tutorial java/Biodata/src/com/arya/Main.java
@@ -0,0 +1,11 @@
+package com.arya;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : ARYA YUDHA PRATAMA");
+        System.out.println("Kelamin : Laki-laki");
+        System.out.println("Alamat : Prum.Bukit Alam Blok Q-16 Suwayuwo-Sukorejo");
+        System.out.println("Jurusan : Rekayasa Perangkat Lunak (RPL)");
+    }
+}
