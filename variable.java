95ec9d44f933aa993bbf7bc49c0f13e4cf3aca4e
aryayudhapratama <69952993+aryayudhapratama@users.noreply.github.com>
2020-09-03 11:02:29 +0900

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,43 @@
+package com.variabel;
+
+public class variable {
+    public static void main(String[] args) {
+        //Luas segitiga
+        System.out.println("==== LUAS SEGITIGA ====");
+        double set = 0.5;
+        double alas = 20;
+        double tinggi = 20;
+        double hasil;
+        //Penyelesaian
+        hasil = set * alas * tinggi;
+        System.out.println("Setengah" + " = " + set);
+        System.out.println("Alas" +  " = " + alas);
+        System.out.println("Tinggi" + " = " + tinggi);
+        System.out.println(set + " x " + alas + " x " + tinggi + " = " + hasil);
+        //AKhir code luas segitiga
+
+        //Luas lingkaran
+        System.out.println("==== LUAS LINGKARAN ====");
+        double phi = 3.14;
+        double jari = 10;
+        double jawab;
+        //Penyelesaian
+        jawab = phi * jari;
+        System.out.println("Phi" + " = " + phi);
+        System.out.println("Jari_jari" + " = " + jari);
+        System.out.println(phi + " x " + jari + " = " + jawab);
+        //Akhir code luas lingkaran
+
+        //luas persegi
+        System.out.println("==== LUAS PERSEGI PANJANG ====");
+        double p = 10;
+        double l = 15;
+        double jadi;
+        //Penyelesaian
+        jadi = p * l;
+        System.out.println("Panjang" + " = " + p);
+        System.out.println("Lebar" + " = " + l);
+        System.out.println(p + " x " + l + " = " + jadi);
+        
+    }
+}
